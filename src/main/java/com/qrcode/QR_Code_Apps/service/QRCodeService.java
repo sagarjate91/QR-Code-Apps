185cddcb3c2d94522022f1de5e9e79f340f85399
sagarjate91 <jate.sagar91@gmail.com>
2025-08-15 12:28:32 +0530

    Added Some code

--- src/main/java/com/qrcode/QR_Code_Apps/service/QRCodeService.java
+++ src/main/java/com/qrcode/QR_Code_Apps/service/QRCodeService.java
@@ -1,12 +1,18 @@
 package com.qrcode.QR_Code_Apps.service;
 
 import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -49,7 +55,7 @@ public class QRCodeService {
         int height = 500;
 
         // Create QRCode directory if it doesn't exist
-        java.io.File directory = new java.io.File("C:\\QRCode");
+        File directory = new File("C:\\QRCode");
         if (!directory.exists()) {
             directory.mkdirs();
         }
@@ -67,20 +73,7 @@ public class QRCodeService {
         }
     }
 
-    public boolean isValidQRCode(String filePath, String fileName) {
-        try {
-            log.info("Validating QR Code from file: " + filePath);
-            log.info("Expected fileName: " + fileName);
-            String qr1 = getQR(filePath);
-            log.info("Decoded QR Code text: " + qr1);
-            String qr2 = getQR("c:\\QRCode\\" + fileName + ".png");
-            log.info("Decoded QR Code text: " + qr2);
-            return qr1.equals(qr2);
-        } catch (Exception e) {
-            log.error("Error validating QR Code", e);
-            return false;
-        }
-    }
+
 
     /**
      * Get the file path of a user's QR code
@@ -93,26 +86,26 @@ public class QRCodeService {
      * Check if QR code exists for a user
      */
     public boolean qrCodeExists(String mobile) {
-        java.io.File file = new java.io.File(getUserQRCodePath(mobile));
+        File file = new File(getUserQRCodePath(mobile));
         return file.exists();
     }
 
     /**
      * Decode QR code from uploaded file and extract user information
      */
-    public String decodeQRCodeFromFile(org.springframework.web.multipart.MultipartFile file) {
+    public String decodeQRCodeFromFile(MultipartFile file) {
         try {
             // Convert MultipartFile to BufferedImage
-            java.awt.image.BufferedImage bufferedImage = javax.imageio.ImageIO.read(file.getInputStream());
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
 
             if (bufferedImage == null) {
                 log.error("Could not read image from uploaded file");
                 return null;
             }
 
-            LuminanceSource source = new com.google.zxing.client.j2se.BufferedImageLuminanceSource(bufferedImage);
+            LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
             BinaryBitmap bitmap = new BinaryBitmap(new com.google.zxing.common.HybridBinarizer(source));
-            Result result = new com.google.zxing.qrcode.QRCodeReader().decode(bitmap);
+            Result result = new QRCodeReader().decode(bitmap);
 
             log.info("Successfully decoded QR code: " + result.getText());
             return result.getText();
@@ -168,7 +161,7 @@ public class QRCodeService {
      * Read and decode QR code from file path (public method for UserService)
      */
     public String getQR(String filePath) throws IOException, NotFoundException, ChecksumException, FormatException {
-        java.awt.image.BufferedImage bufferedImage = javax.imageio.ImageIO.read(new java.io.File(filePath));
+        BufferedImage bufferedImage = ImageIO.read(new java.io.File(filePath));
         LuminanceSource source = new com.google.zxing.client.j2se.BufferedImageLuminanceSource(bufferedImage);
         BinaryBitmap bitmap = new BinaryBitmap(new com.google.zxing.common.HybridBinarizer(source));
         Result result = new com.google.zxing.qrcode.QRCodeReader().decode(bitmap);
